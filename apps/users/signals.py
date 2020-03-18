377f105dac85ba6035637dba3e03630a1771c2e4
batTrung <trungbat.pv@gmail.com>
2020-03-18 11:21:25 +0700

    Adding Multiple languages success!

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,8 +1,9 @@
 from allauth.account.signals import password_changed
+from django.utils.translation import ugettext as _
 from django.dispatch import receiver
 from django.contrib import messages
 
 
 @receiver(password_changed)
 def password_change_callback(sender, request, user, **kwargs):
-    messages.success(request, 'You have Successfully changed your Password!.', extra_tags="password_changed")
+    messages.success(request, _('You have Successfully changed your Password!.'), extra_tags="password_changed")
